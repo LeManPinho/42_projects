216cbf13ac318d1ba6137fd4a645261ad3c2c615
LeManPinho <moonsheik@gmail.com>
2016-04-05 16:08:53 +0200

    minishell start

---
+++ minishell/minishell.h
@@ -0,0 +1,15 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <sys/types.h>
+# include <dirent.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/stat.h>
+# include <fcnt1.h>
+# include <sys/wait.h>
+# include <sys/time.h>
+# include <sys/resource.h>
+# include <signal.h>
+
+#endif
