e0fbd03a1c58fa54652e65e6cfbb82a65aea6360
Hippolyte Jacque <hippolytejacque@hotmail.fr>
2016-04-07 15:08:48 +0200

    Update minishell.h
    fonctions

--- minishell/minishell.h
+++ minishell/minishell.h
@@ -11,11 +11,16 @@
 # include <sys/time.h>
 # include <sys/resource.h>
 # include <signal.h>
+# include "libft/libft.h"
 
 typedef struct  s_list
 {
   void          *data;
   struct s_list *next;
 }               t_list;
+
+t_list	*addelem(t_list **begin, void *data);
+t_list  *dtab_to_lst(char **dtab);
+char    *ft_getenvpath(char **env, char *elem);
 
 #endif
